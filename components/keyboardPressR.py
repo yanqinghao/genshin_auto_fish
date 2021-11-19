60472277236d5ccbcd1996cc3d0b5574edee8c4d
yanqinghao <woshiyanqinghao@gmail.com>
2021-11-19 23:41:07 +0800

    fix

--- components/keyboardPressR.py
+++ components/keyboardPressR.py
@@ -1,31 +1,20 @@
-# -*- coding: utf-8 -*-
+-*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
 
-import suanpan
 import keyboard
 from suanpan.app import app
-from suanpan.arguments import String
 from suanpan.log import logger
-from utils import *
 
-
-restore_saved_module("threading")
 
 def pressGenerator():
     logger.info("Start to detect keyboard R...")
     keyboard.wait('r')
     logger.info("Detect keyboard R Pressed...")
-    yield "r"
+    app.sendWithoutContext("r")
     logger.info("Send Message to Next Node")
     while True:
         pass
 
 
-@app.trigger.loop(pressGenerator)
-@app.trigger.output(String(key="outputData1"))
-def keyboardPressR(_, i):
-    return i
-
-
 if __name__ == "__main__":
-    suanpan.run(app)
+    pressGenerator()
